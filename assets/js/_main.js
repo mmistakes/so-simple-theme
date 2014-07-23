f07ae0f4769f7b8fbcbbc37fcc419339142c7d0d
Michael Rose <est.michael@gmail.com>
2014-07-23 08:18:43 -0400

    Add Magnific Popup - Image links now display in a lightbox

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -27,3 +27,26 @@ $('#site-nav').click(function(event){
 $(function() {
 	$("article").fitVids();
 });
+
+// Add lightbox class to all image links
+$("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+
+// Magnific-Popup options
+$(document).ready(function() {
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+    },
+    removalDelay: 300, // Delay in milliseconds before popup is removed
+    // Class that is added to body when popup is open. 
+    // make it unique to apply your CSS animations just to this exact popup
+    mainClass: 'mfp-fade'
+  });
+});
