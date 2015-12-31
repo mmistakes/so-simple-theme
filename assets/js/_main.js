fb79731dc34f9530ceb283136e7f6f98861d37b2
Matthew Vickery <vickery.matthew@gmail.com>
2015-12-31 08:32:06 +0100

    removed navigation js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,30 +1,5 @@
 /*! Plugin options and other jQuery stuff */
 
-// Responsive Nav
-var navigation = responsiveNav("#site-nav", { // Selector: The ID of the wrapper
-  animate: true, // Boolean: Use CSS3 transitions, true or false
-  transition: 200, // Integer: Speed of the transition, in milliseconds
-  label: "<i class='fa fa-bars'></i> Menu", // String: Label for the navigation toggle
-  insert: "before", // String: Insert the toggle before or after the navigation
-  customToggle: "", // Selector: Specify the ID of a custom toggle
-  openPos: "relative", // String: Position of the opened nav, relative or static
-  jsClass: "js", // String: 'JS enabled' class which is added to <html> el
-  init: function(){}, // Function: Init callback
-  open: function(){}, // Function: Open callback
-  close: function(){} // Function: Close callback
-});
-
-$('html').click(function() {
-  //Hide the menus if visible
-  if ($(navigation.wrapper).hasClass('opened')) {
-  	navigation.toggle();
-  }
-});
-
-$('#site-nav').click(function(event){
-    event.stopPropagation();
-});
-
 // FitVids options
 $(function() {
 	$("article").fitVids();
