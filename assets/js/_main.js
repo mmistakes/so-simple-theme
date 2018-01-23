cfa22b07ef8c03f51da18cf08dea7c93eb992108
Michael Rose <est.michael@gmail.com>
2018-01-23 08:51:08 -0500

    Remove duplicate `js` class form <html> element

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -8,46 +8,46 @@ var navigation = responsiveNav("#site-nav", { // Selector: The ID of the wrapper
   insert: "before", // String: Insert the toggle before or after the navigation
   customToggle: "", // Selector: Specify the ID of a custom toggle
   openPos: "relative", // String: Position of the opened nav, relative or static
-  jsClass: "js", // String: 'JS enabled' class which is added to <html> el
-  init: function(){}, // Function: Init callback
-  open: function(){}, // Function: Open callback
-  close: function(){} // Function: Close callback
+  jsClass: "", // String: 'JS enabled' class which is added to <html> el
+  init: function () { }, // Function: Init callback
+  open: function () { }, // Function: Open callback
+  close: function () { } // Function: Close callback
 });
 
-$('html').click(function() {
+$('html').click(function () {
   //Hide the menus if visible
   if ($(navigation.wrapper).hasClass('opened')) {
-  	navigation.toggle();
+    navigation.toggle();
   }
 });
 
-$('#site-nav').click(function(event){
-    event.stopPropagation();
+$('#site-nav').click(function (event) {
+  event.stopPropagation();
 });
 
 // FitVids options
-$(function() {
-	$("article").fitVids();
+$(function () {
+  $("article").fitVids();
 });
 
 // Add lightbox class to all image links
 $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
 // Magnific-Popup options
-$(document).ready(function() {
+$(document).ready(function () {
   $('.image-popup').magnificPopup({
     type: 'image',
     tLoading: 'Loading image #%curr%...',
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
     },
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
     removalDelay: 300, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open. 
+    // Class that is added to body when popup is open.
     // make it unique to apply your CSS animations just to this exact popup
     mainClass: 'mfp-fade'
   });
