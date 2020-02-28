1cf2a1bc5b3f25437daba61df14b20dbc7db6161
Santiago Montesdeoca <santymontesdeoca@gmail.com>
2020-02-28 15:58:15 +0200

    Yearly-montly toggle

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -25,3 +25,35 @@ $(function(){
     no_overlay: true, //Do not show the overlay with before and after
   });
 });
+
+
+// pricing togglers
+var e = document.getElementById("filt-monthly"),
+    d = document.getElementById("filt-yearly"),
+    t = document.getElementById("switcher"),
+    m = document.getElementById("monthly"),
+    y = document.getElementById("yearly");
+
+
+e.addEventListener("click", function(){
+  t.checked = false;
+  e.classList.add("toggler--is-active");
+  d.classList.remove("toggler--is-active");
+  m.classList.remove("hide");
+  y.classList.add("hide");
+});
+
+d.addEventListener("click", function(){
+  t.checked = true;
+  d.classList.add("toggler--is-active");
+  e.classList.remove("toggler--is-active");
+  m.classList.add("hide");
+  y.classList.remove("hide");
+});
+
+t.addEventListener("click", function(){
+  d.classList.toggle("toggler--is-active");
+  e.classList.toggle("toggler--is-active");
+  m.classList.toggle("hide");
+  y.classList.toggle("hide");
+})
