3a290fe37c1c4c665f1c1d606195006248f9b207
Khedidja Seridi <khedidja.seridi@umontreal.ca>
2021-08-17 14:28:47 -0400

    add logos and misc changes

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Introduction",
-        "excerpt":"This post has YAML Front Matter of hidden: true and should not appear in paginator.posts. ","categories": [],
+        "excerpt":"La transformation numérique en santé est bien entamée dans notre société et son contexte entier doit être considéré pour une transition optimale. D’une part, la transformation numérique découlant des découvertes technologiques et des avancés algorithmiques ont pour but d’améliorer les soins aux patients. En exemple, on pense bien sûr d’emblée...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/Introduction/"
       },{
