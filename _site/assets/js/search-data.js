9b8cf173142f39638d39ec6c41de08cc5aeba14f
Khedidja Seridi <khedidja.seridi@umontreal.ca>
2021-08-17 16:57:26 -0400

    remove the baseurl

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Introduction",
         "excerpt":"Introduction ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/blog/Introduction/"
+        "url": "http://localhost:4000/Introduction/"
       },{
         "title": "Interoperabilité",
         "excerpt":"Un petit test ","categories": ["Layout"],
         "tags": ["Intéropérabilité","standards","santé"],
-        "url": "http://localhost:4000/blog/layout/test-avant-deploiment/"
+        "url": "http://localhost:4000/layout/test-avant-deploiment/"
       }]
