c0bc73a6aa96c42836b66bbd3f238214b6e79aa6
Khedidja Seridi <khedidja.seridi@umontreal.ca>
2021-08-17 16:49:13 -0400

    add blog to url

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Introduction",
         "excerpt":"La transformation numérique en santé est bien entamée dans notre société et son contexte entier doit être considéré pour une transition optimale. D’une part, la transformation numérique découlant des découvertes technologiques et des avancés algorithmiques ont pour but d’améliorer les soins aux patients. En exemple, on pense bien sûr d’emblée...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Introduction/"
+        "url": "http://localhost:4000/blog/Introduction/"
       },{
         "title": "Interoperabilité",
         "excerpt":"Un petit test ","categories": ["Layout"],
         "tags": ["Intéropérabilité","standards","santé"],
-        "url": "http://localhost:4000/layout/test-avant-deploiment/"
+        "url": "http://localhost:4000/blog/layout/test-avant-deploiment/"
       }]
