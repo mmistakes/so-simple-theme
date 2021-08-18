09d09e7f93f68e1b06ba8c25ed97027e165e4232
Khedidja Seridi <khedidja.seridi@umontreal.ca>
2021-08-18 15:10:57 -0400

    adjust desciption

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Introduction",
         "excerpt":"Introduction ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Introduction/"
+        "url": "https://sante-numerique.github.io/Introduction/"
       },{
         "title": "Interoperabilité",
         "excerpt":"Un petit test ","categories": ["Layout"],
         "tags": ["Intéropérabilité","standards","santé"],
-        "url": "http://localhost:4000/layout/test-avant-deploiment/"
+        "url": "https://sante-numerique.github.io/layout/test-avant-deploiment/"
       }]
