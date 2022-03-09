5c3233d7826241d672c16c45c653059733b39de9
cyrstem <cyrstem@gmail.com>
2022-03-08 19:33:51 -0500

    non comercial setup

--- assets/js/mnprx.js
+++ assets/js/mnprx.js
@@ -1,7 +1,11 @@
 
 const comercial = document.getElementById("comercial");
 const nonComercial = document.getElementById("noncomercial");
+const individual = document.getElementById("individual")
+const nonprofit = document.getElementById("nonprofit")
 
+const community = document.getElementById("community ")
+const patron = document.getElementById("patron")
 
 comercial.addEventListener("click", function (){
   console.log("click comercial")
@@ -9,4 +13,25 @@ comercial.addEventListener("click", function (){
 
 nonComercial.addEventListener("click",function(){
   console.log("click noncomercial")
+
+  comercial.classList.add("hide");
+  nonComercial.classList.add("hide");
+  individual.classList.remove("hide");
+  nonprofit.classList.remove("hide")
+})
+
+nonprofit.addEventListener("click",function(){
+  console.log("click nonprofit")
+  individual.classList.add("hide");
+  nonprofit.classList.add("hide");
+patron.classList.remove("hide")
+})
+
+individual.addEventListener("click",function(){
+  console.log("click individual");
+
+  individual.classList.add("hide");
+  nonprofit.classList.add("hide");
+  community.classList.remove("hide");
+
 })
